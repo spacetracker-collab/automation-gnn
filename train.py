d96ddacea99c45cc55571ecd8aee64ac0275753b
SpaceTracker <83755715+spacetracker-collab@users.noreply.github.com>
2026-03-15 21:07:56 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn.functional as F
+from model import AutomationGNN
+from dataset import generate_automation_graph
+
+data = generate_automation_graph()
+
+model = AutomationGNN(
+    input_dim=5,
+    hidden_dim=32,
+    output_dim=1
+)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+for epoch in range(200):
+
+    model.train()
+    optimizer.zero_grad()
+
+    pred = model(data.x, data.edge_index).squeeze()
+
+    loss = F.mse_loss(pred, data.y)
+
+    loss.backward()
+    optimizer.step()
+
+    if epoch % 20 == 0:
+        print("Epoch:", epoch, "Loss:", loss.item())
